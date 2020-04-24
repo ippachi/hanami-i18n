c91544337f7911cd02d0a75b2267df8cebdb1a1e
ippachi <ippachi1018@gmail.com>
2020-04-24 14:31:54 +0000

    Add install command

--- lib/hanami/i18n.rb
+++ lib/hanami/i18n.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
 require 'hanami/i18n/version'
-require 'hanami/i18n/entity'
-require 'hanami/i18n/helpers/form_helper/form_builder'
-require 'hanami/i18n/view'
-
-::Hanami::View.configure do
-  prepare do
-    include Hanami::I18n::View
-  end
-end
+require 'hanami/i18n/cli'

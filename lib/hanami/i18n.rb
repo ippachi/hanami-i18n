39a6a7618d551203d4486cedb49fa742134f240b
ippachi <ippachi1018@gmail.com>
2020-04-29 05:48:53 +0000

    Require i18n settings

--- lib/hanami/i18n.rb
+++ lib/hanami/i18n.rb
@@ -3,3 +3,7 @@
 require 'hanami/i18n/version'
 require 'hanami/i18n/helpers/form'
 require 'hanami/i18n/validations'
+
+if Hanami.respond_to?(:root)
+  require Hanami.root.join('config/locale.rb')
+end
